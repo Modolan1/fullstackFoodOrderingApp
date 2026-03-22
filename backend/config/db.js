1aa09bc675ebb9c6e0b498893fb6dcd9380ab72f
Sukurat Modupe Ogunmuyiwa <modupeogunmuyiwa85@gmail.com>
2026-03-21 21:32:25 -0500

    Update MongoDB URI retrieval method

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
  export const connectDB = async() =>{
-    const mongoUri = String(process.env.MONGODB_URI || 'mongodb+srv://modolan:12345678a@cluster0.avbn0r1.mongodb.net/food-delivery').trim()
+    const mongoUri = String(process.env.MONGODB_URI).trim()
     await mongoose.connect(mongoUri).then( ()=>console.log("DB connected" ))
 }
